05a88ff21a19a5d01078e31579885c9c23786122
Mohannadk28 <73769579+Mohannadk28@users.noreply.github.com>
2023-02-13 21:31:24 +0300

    emojis

--- config.def.h
+++ config.def.h
@@ -5,7 +5,10 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "FiraCode Nerd Font:pixelsize=16:antialias=true:autohint=true";
+static char *font = {"FiraCode Nerd Font:pixelsize=16:antialias=true:autohint=true",
+                     "JoyPixels:size=10:antialias=true:autohint=true"
+};
+
 static int borderpx = 1;
 
 /*
