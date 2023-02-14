f7ff400ffb2eba2fd26c8fda4401a53a32e1e473
Mohannadk28 <73769579+Mohannadk28@users.noreply.github.com>
2023-02-14 14:56:16 +0300

    removed config.h

--- config.def.h
+++ config.def.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = {"FiraCode Nerd Font:pixelsize=16:antialias=true:autohint=true",
-                     "JoyPixels:size=10:antialias=true:autohint=true"
+                     "JoyPixels:size=16:antialias=true:autohint=true"
 };
 
 static int borderpx = 1;
